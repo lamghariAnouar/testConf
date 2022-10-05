580f83ffbc1fb64a522ae5cc268853c03dca8cc9
lamghariAnouar <lamgharianouarfstg@gmail.com>
2022-10-05 16:00:41 +0200

    sc

--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,6 @@
 public class Test {
     public static void main(String[] args) {
         System.out.println("premiere ligne");
+        System.out.println("deuxieme  ligne ou troisieme ligne");
     }
 }
